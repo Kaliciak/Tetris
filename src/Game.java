77d5180fa6d7221bde3c37ea5095ccaed6bcaad6
Piotrek <piotr-kaliciak@wp.pl>
2020-12-25 19:23:33 +0100

    Constructing project structure

---
+++ src/Game.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Game extends Application {
+    public static void main(String[] args) {
+        System.out.println("TETRIS");
+    }
+
+    @Override
+    public void start(Stage primaryStage){
+
+    }
+}
