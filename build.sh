aa446bf2a9111134e4ac5775280f75bd76d07918
kaliciak <piotr-kaliciak@wp.pl>
2021-01-16 19:13:06 +0100

    Imrpoved build scripts

--- build.sh
+++ build.sh
@@ -1,2 +1,15 @@
-javac --module-path $PATH_TO_FX --add-modules=javafx.controls --add-modules=javafx.fxml --add-modules=javafx.media -d out $(find src -name "*.java")
+#!/bin/bash
+
+build="$PATH_TO_FX"
+
+if [ "$#" -gt "0" ]; then
+    build="$1";
+else
+    if [ "$PATH_TO_FX" = "" ]; then
+        echo "You need to set \$PATH_TO_FX variable or pass path to javafx library as argument";
+        exit
+    fi
+fi
+
+javac --module-path $build --add-modules=javafx.controls --add-modules=javafx.fxml --add-modules=javafx.media -d out $(find src -name "*.java")
 cp -r Resources/* out/
--- run.sh
+++ run.sh
@@ -1,1 +1,14 @@
-java --module-path $PATH_TO_FX --add-modules=javafx.controls --add-modules=javafx.fxml --add-modules=javafx.media -cp out Tetris.Game
+#!/bin/bash
+
+build="$PATH_TO_FX"
+
+if [ "$#" -gt "0" ]; then
+    build="$1";
+else
+    if [ "$PATH_TO_FX" = "" ]; then
+        echo "You need to set \$PATH_TO_FX variable or pass path to javafx library as argument";
+        exit
+    fi
+fi
+
+java --module-path $build --add-modules=javafx.controls --add-modules=javafx.fxml --add-modules=javafx.media -cp out Tetris.Game
